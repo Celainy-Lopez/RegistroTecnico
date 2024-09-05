27d737515afd0787c8f6fb088f5e40ae66a0ecf2
celainy-lopez <celainy_lopez@ucne.edu.do>
2024-09-05 17:31:42 -0400

    Implemente el atributo range para permitir la correcta validación del sueldo además arregle errores ortograficos en el mensaje de ErrorMessage. #5

--- Models/Tecnicos.cs
+++ Models/Tecnicos.cs
@@ -6,10 +6,11 @@ namespace RegistroTecnico.Models
         [Key]
         public int tecnicoId { get; set; }
 
-        [Required(ErrorMessage = "Por favor ingrese el nombre del Tecnico")]
+        [Required(ErrorMessage = "Por favor ingrese el nombre del técnico")]
         public string? nombreTecnico { get; set; }
 
-        [Required(ErrorMessage = "Por favor ingrese el sueldo del tecnico")]
-        public float sueldoHora { get; set; }
+        [Required(ErrorMessage = "Por favor ingrese el sueldo del técnico")]
+        [Range(0.01, float.MaxValue, ErrorMessage = "El sueldo debe ser mayor que 0")]
+        public float? sueldoHora { get; set; }
     }
 }
