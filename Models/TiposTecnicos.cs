87067823c5188dce82e606b6136785b1d5591f21
celainy-lopez <celainy_lopez@ucne.edu.do>
2024-09-06 11:25:22 -0400

    Creación del modelo TiposTecnicos

---
+++ Models/TiposTecnicos.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnico.Models;
+
+public class TiposTecnicos
+{
+	[Key]
+	public int TipoTecnicoId { get; set; }
+
+	[Required(ErrorMessage = "Por favor ingrese una descripción valida")]
+	public string Descripcion { get; set; }
+}
