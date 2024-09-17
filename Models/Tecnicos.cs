5cd9bf8eaf77a672ce4007c01e9e14977dafbbf1
celainy-lopez <celainy_lopez@ucne.edu.do>
2024-09-17 13:00:44 -0400

    Modificación del modelo Tecnicos para establecer una relación entre Tecnicos y TiposTecnicos

--- Models/Tecnicos.cs
+++ Models/Tecnicos.cs
@@ -1,10 +1,13 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace RegistroTecnico.Models;
 
 public class Tecnicos
 {
     [Key]
     public int TecnicoId { get; set; }
+
 
     [Required(ErrorMessage = "Por favor ingrese el nombre del técnico")]
     public string? NombreTecnico { get; set; }
@@ -13,6 +16,9 @@ public class Tecnicos
     [Range(0.01, float.MaxValue, ErrorMessage = "El sueldo debe ser mayor que 0")]
     public float? SueldoHora { get; set; }
 
-    [Required(ErrorMessage = "Debe seleccionar una descripción")]
-    public string Descripcion { get; set; }
+    [ForeignKey("TipoTecnico")]
+    [Range(1, int.MaxValue, ErrorMessage = "Por favor, seleccione una opción válida")]
+    public int TipoTecnicoId { get; set; }
+
+    public TiposTecnicos? TipoTecnico { get; set; }
 }
