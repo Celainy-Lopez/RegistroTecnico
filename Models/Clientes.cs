fb18741b0206ddbc3f35da50ed5ff87b6c8576ab
celainy-lopez <celainy_lopez@ucne.edu.do>
2024-09-17 12:15:07 -0400

    Modificación del namespace

--- Models/Clientes.cs
+++ Models/Clientes.cs
@@ -1,18 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace RegistroTecnico.Models
+namespace RegistroTecnico.Models;
+
+public class Clientes
 {
-	public class Clientes
-	{
-		[Key]
-		public int ClienteId { get; set; }
+	[Key]
+	public int ClienteId { get; set; }
 
-		[Required(ErrorMessage ="Ingrese un nombre valido")]
-		public string Nombres { get; set; }
+	[Required(ErrorMessage ="Ingrese un nombre valido")]
+	public string Nombres { get; set; }
 
-		[Required(ErrorMessage ="Ingrese un whatsApp valido")]
+	[Required(ErrorMessage ="Ingrese un whatsApp valido")]
         [RegularExpression(@"^\d{10}$", ErrorMessage = "El número de teléfono debe tener exactamente 10 dígitos.")]
         public string? WhatsApp {  get; set; }
 
-	}
 }
