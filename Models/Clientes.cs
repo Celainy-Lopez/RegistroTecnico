b07b842377e8702c57e09c12f8c38c08bbe3a356
celainy-lopez <celainy_lopez@ucne.edu.do>
2024-09-17 12:13:27 -0400

    Agregue el modelo Clientes con sus requerimientos

---
+++ Models/Clientes.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnico.Models
+{
+	public class Clientes
+	{
+		[Key]
+		public int ClienteId { get; set; }
+
+		[Required(ErrorMessage ="Ingrese un nombre valido")]
+		public string Nombres { get; set; }
+
+		[Required(ErrorMessage ="Ingrese un whatsApp valido")]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "El número de teléfono debe tener exactamente 10 dígitos.")]
+        public string? WhatsApp {  get; set; }
+
+	}
+}
