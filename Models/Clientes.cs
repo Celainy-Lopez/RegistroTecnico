42f376caca484a77292990ff7fa8233c41c8e957
celainy-lopez <celainy_lopez@ucne.edu.do>
2024-10-03 20:41:25 -0400

    Corrigiendo identación

--- Models/Clientes.cs
+++ Models/Clientes.cs
@@ -11,7 +11,7 @@ public class Clientes
 	public string Nombres { get; set; }
 
 	[Required(ErrorMessage ="Ingrese un whatsApp valido")]
-        [RegularExpression(@"^\d{10}$", ErrorMessage = "El número de teléfono debe tener exactamente 10 dígitos.")]
-        public string? WhatsApp {  get; set; }
+    [RegularExpression(@"^\d{10}$", ErrorMessage = "El número de teléfono debe tener exactamente 10 dígitos.")]
+    public string? WhatsApp {  get; set; }
 
 }
