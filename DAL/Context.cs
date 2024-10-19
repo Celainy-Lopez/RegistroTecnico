339e7b909a85551c984b5d27bb75293d519a39f9
celainy-lopez <celainy_lopez@ucne.edu.do>
2024-10-18 21:36:01 -0400

    Generando el OnModelCreating para Articulos

--- DAL/Context.cs
+++ DAL/Context.cs
@@ -15,4 +15,17 @@ public class Context(DbContextOptions<Context> options) : DbContext(options)
 	public DbSet<Trabajos> Trabajos { get; set; }
 
 	public DbSet<Prioridades> Prioridades { get; set; }
+
+	public DbSet<Articulos> Articulos { get; set; }
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		base.OnModelCreating(modelBuilder);
+		modelBuilder.Entity<Articulos>().HasData(new List<Articulos>()
+		{
+			new Articulos() {ArticuloId = 1,Descripcion = "Multimetro", Costo = 2000, Precio = 5000, Existencia = 15},
+			new Articulos() {ArticuloId = 2,Descripcion = "Cable de redes", Costo = 250, Precio = 500, Existencia = 25},
+			new Articulos() {ArticuloId = 3,Descripcion = "Pinzas", Costo = 500, Precio = 750, Existencia = 50}
+		});
+	}
 }
